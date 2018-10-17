c5f4195d2a57e92203a2343c1e2fd9e1613f7e7b
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 09:34:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,10 @@ client.on('message', message => {
 	} else if (message.content === '.download') {
 		 message.channel.send('Sorry the app hasn\'t been released yet');
 	} else if (message.content === 'ApolloBot Did you eat at taco bell?') {
-		const poop = client.emojis.find(emoji => emoji.name === "poop");
-		message.channel.send(`💩`);
+		const poop = client.emojis.find(emoji => emoji.name === ":poop:");
+		message.channel.send(`${poop}`);
 	} else if (themessage.includes('socks')) {
-		if (themessage !== "\:socks: \:socks: is it cold? \:socks: \:socks:") {
+		if (themessage !== ":socks: \:socks: is it cold? \:socks: \:socks:") {
 			message.channel.send('\:socks: \:socks: is it cold? \:socks: \:socks:');
 		}
 	}
