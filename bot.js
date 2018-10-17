77d27d1b30253f1282564aba382efefb56ca4afc
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 08:30:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,23 @@ const client = new Discord.Client();
 client.on('ready', () => {
 	console.log('I am ApolloBOT and I am ready to serve you!'); 
 });
-client.on('message', message => {
-	if (message.content === '.todo') {
-		message.reply('https://apollotv.xyz/todo/');
-	}
-	if (message.content === 'socks') {
-		message.reply(' socks socks  is it cold? socks socks');
-	}
+client.on('message', function (user, userID, channelID, message, evt) {
+    // let's listen for messages that will start with `.`
+    if (message.substring(0, 1) == '.') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // .todo
+            case 'todo':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'https://apollotv.xyz/todo/'
+                });
+            break;
+            // end of functions
+         }
+     }
 });
 client.login(process.env.BOT_TOKEN);
