5f35cccc7c94ffc7b926ba2fb0ec69e2b9a8a0ea
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 02:12:59 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ApolloBOT and I am ready to serve you!');
-});
-
-client.on('message', message => {
-    if (message.content === '.todo') {
-        message.reply('https://apollotv.xyz/todo/');
-    }
-});
-
+client.on('ready', () => { console.log('I am ApolloBOT and I am ready to serve you!'); });
+client.on('message', message => { if (message.content === '.todo') { message.reply('https://apollotv.xyz/todo/'); } });
 client.login(process.env.BOT_TOKEN);
