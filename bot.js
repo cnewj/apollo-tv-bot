bb437b68608e2cd610d9221c39fde91fb30d0d77
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 02:07:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+const Discord = require('discord.js');
+const client = Discord.Client();
 
-// Configure logger settings
-/*logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+    console.log('I am ApolloBOT and I am ready to serve you!');
 });
-logger.level = 'debug';*/
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
+
+client.on('message', message => {
+    if (message.content === '.todo') {
+        message.reply('https://apollotv.xyz/todo/');
+    }
 });
-bot.on('ready', function (evt) {
-/*    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');*/
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '.') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !todo
-            case 'todo':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'https://apollotv.xyz/todo/'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+client.login(process.env.BOT_TOKEN);
