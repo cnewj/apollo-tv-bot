265b8768b7ef327cb58d127a21d7fe51b52a9e4e
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 09:00:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,15 @@ client.on('ready', () => {
 	console.log('I am ApolloBOT and I am ready to serve you!'); 
 });
 client.on('message', message => {
+	var themessage = message.content;
 	if (message.content === '.todo') {
 		 message.channel.send('https://apollotv.xyz/todo/');
-	}
-	if (message.content === '.download') {
+	} else if (message.content === '.download') {
 		 message.channel.send('Sorry the app hasn\'t been released yet');
-	}
-	if (message.content.includes('socks')) {
-		 message.channel.send(' socks socks  is it cold? socks socks');
+	} else if (themessage.includes('socks')) {
+		if (themessage !== "socks socks  is it cold? socks socks") {
+			message.channel.send('socks socks  is it cold? socks socks');
+		}
 	}
 });
 client.login(process.env.BOT_TOKEN);
