10d1c31fe30dda37982e38a4c5d5c53678dcf556
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 09:37:32 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ client.on('message', message => {
 		 message.channel.send('https://apollotv.xyz/todo/');
 	} else if (message.content === '.download') {
 		 message.channel.send('Sorry the app hasn\'t been released yet');
-	} else if (message.content === 'ApolloBot Did you eat at taco bell?') {
-		const poop = client.emojis.find(emoji => emoji.name === ":poop:");
-		message.channel.send(`${poop}`);
+	} else if (message.content === 'ApolloBot what are you wearing?') {
+		const sock = client.emojis.get("491220639374508032");
+		message.channel.send(`${sock}`);
 	} else if (themessage.includes('socks')) {
 		if (themessage !== ":socks: \:socks: is it cold? \:socks: \:socks:") {
 			message.channel.send('\:socks: \:socks: is it cold? \:socks: \:socks:');
