3438f7705ec1dc344eff52137b68a9d9324a7125
cnewj <44225097+cnewj@users.noreply.github.com>
2018-10-17 08:39:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,21 +5,15 @@ client.on('ready', () => {
 });
 client.on('message', function (user, userID, channelID, message, evt) {
     // let's listen for messages that will start with `.`
-    if (message.substring(0, 1) == '.') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // .todo
-            case 'todo':
-                client.sendMessage({
-                    to: channelID,
-                    message: 'https://apollotv.xyz/todo/'
-                });
-            break;
-            // end of functions
-         }
-     }
+	switch(message.content) {
+	    // .todo
+	    case 'todo':
+		client.sendMessage({
+		    to: channelID,
+		    message: 'https://apollotv.xyz/todo/'
+		});
+	    break;
+	    // end of functions
+	 }
 });
 client.login(process.env.BOT_TOKEN);
